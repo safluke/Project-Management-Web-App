1afbbb4dd4d3bcd2055429b2517e21fcd17e45a1
safluke <saf_2356@outlook.com>
2022-08-31 10:32:52 +0100

    updated pom with main class

--- src/main/java/com/qa/projectManagementApp/ProjectMangementApp.java
+++ src/main/java/com/qa/projectManagementApp/ProjectMangementApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectMangementAppApplication {
+public class ProjectMangementApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectMangementAppApplication.class, args);
+		SpringApplication.run(ProjectMangementApp.class, args);
 	}
 
 }
--- src/test/java/com/qa/projectManagementApp/ProjectMangementAppTests.java
+++ src/test/java/com/qa/projectManagementApp/ProjectMangementAppTests.java
@@ -3,8 +3,8 @@ package com.qa.projectManagementApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes=ProjectMangementAppApplicationTests.class)
-class ProjectMangementAppApplicationTests {
+@SpringBootTest(classes=ProjectMangementAppTests.class)
+class ProjectMangementAppTests {
 
 	@Test
 	void contextLoads() {
